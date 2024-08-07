2e805c75d6287a4977598955157fde3a18b62ebe
mrAldirs <mr.aldirs@yahoo.com>
2024-08-07 09:17:51 +0700

    ADD href contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,7 +12,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
+import {
+  FaPhoneAlt,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaWhatsapp,
+} from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
 const info = [
@@ -34,6 +39,12 @@ const info = [
 ];
 
 const Contact = () => {
+  const phoneNumber = '+6281529763081';
+  const message = 'Hi, I would like to collaborate with you 😊';
+  const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -47,47 +58,23 @@ const Contact = () => {
         <div className='flex flex-col xl:flex-row gap-[30px]'>
           {/* form */}
           <div className='xl:w-[58%] order-2 xl:order-none'>
-            <form
-              action=''
-              className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'
-            >
+            <div className='flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl'>
               <h3 className='text-3xl text-accent'>
                 Let&apos;s Work Toghether
               </h3>
               <p className='text-white/60'>
                 Contact me if you&apos;re interested in a potential partnership.
+                I look forward to collaborating.
               </p>
-              {/* input */}
-              <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                <Input type='firstname' placeholder='Firstname' />
-                <Input type='lastname' placeholder='Lastname' />
-                <Input type='email' placeholder='Email address' />
-                <Input type='number' placeholder='Phone number' />
-              </div>
-              {/* select */}
-              <Select>
-                <SelectTrigger className='w-full'>
-                  <SelectValue placeholder='Select a service' />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value='odoo'>Odoo Development</SelectItem>
-                    <SelectItem value='web'>Web Development</SelectItem>
-                    <SelectItem value='and'>Android Development</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              {/* textarea */}
-              <Textarea
-                className='h-[120px]'
-                placeholder='Type your message here'
-              />
-              {/* button */}
-              <Button size='md' className='max-w-60'>
-                Send Message
+              <Button
+                size='md'
+                className='max-w-60 flex items-center gap-2'
+                href={whatsappURL}
+              >
+                <span>Send Message</span>
+                <FaWhatsapp className='text-lg' />
               </Button>
-            </form>
+            </div>
           </div>
           {/* info */}
           <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>
