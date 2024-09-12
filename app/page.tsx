41debdc2638a9e4b470afcce4795a147d445f261
mrAldirs <mr.aldirs@yahoo.com>
2024-09-12 11:03:26 +0700

    UPDATE curriculum vitae link

--- app/page.tsx
+++ app/page.tsx
@@ -5,49 +5,49 @@ import { Button } from '@/components/ui/button';
 import { FiDownload } from 'react-icons/fi';
 
 const Home = () => {
-  return (
-    <section className='h-full'>
-      <div className='container mx-auto h-full'>
-        <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-4 xl:pb-10'>
-          {/* text */}
-          <div className='text-center xl:text-left order-2 xl:order-none'>
-            <span className='text-xl'>Programmer</span>
-            <h1 className='h1 mb-3'>
-              Hello, I&apos;m <br />
-              <span className='text-accent'>Aldi Rosid</span>
-            </h1>
-            <p className='max-w-[500px] mb-9 text-white/80'>
-              I excel at crafting elegant digital experiences and I am
-              proficient in various programming languages and technologies.
-            </p>
-            {/* button and socials */}
-            <div className='flex flex-col xl:flex-row  items-center gap-8'>
-              <Button
-                href='https://drive.google.com/file/d/1gyrAHqjXOmuqox5FsNFgYMcm0jljbydX/view?usp=sharing'
-                variant='outline'
-                size='lg'
-                className='uppercase flex items-center gap-2'
-              >
-                <span>Download CV</span>
-                <FiDownload className='text-xl' />
-              </Button>
-              <div className='mb-8 xl:mb-0'>
-                <Social
-                  containerStyles='flex gap-6'
-                  iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500'
-                />
-              </div>
-            </div>
-          </div>
-          {/* photo */}
-          <div className='order-1 xl:order-none mb-8 xl:mb-0'>
-            <Photo />
-          </div>
-        </div>
-      </div>
-      <Stats />
-    </section>
-  );
+	return (
+		<section className='h-full'>
+			<div className='container mx-auto h-full'>
+				<div className='flex flex-col xl:flex-row items-center justify-between xl:pt-4 xl:pb-10'>
+					{/* text */}
+					<div className='text-center xl:text-left order-2 xl:order-none'>
+						<span className='text-xl'>Programmer</span>
+						<h1 className='h1 mb-3'>
+							Hello, I&apos;m <br />
+							<span className='text-accent'>Aldi Rosid</span>
+						</h1>
+						<p className='max-w-[500px] mb-9 text-white/80'>
+							I excel at crafting elegant digital experiences and I am
+							proficient in various programming languages and technologies.
+						</p>
+						{/* button and socials */}
+						<div className='flex flex-col xl:flex-row  items-center gap-8'>
+							<Button
+								href='https://drive.google.com/file/d/151DPU4S5J-QPFdrnJJ0y5haEXE-Dqhkz/view?usp=sharing'
+								variant='outline'
+								size='lg'
+								className='uppercase flex items-center gap-2'
+							>
+								<span>Download CV</span>
+								<FiDownload className='text-xl' />
+							</Button>
+							<div className='mb-8 xl:mb-0'>
+								<Social
+									containerStyles='flex gap-6'
+									iconStyles='w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500'
+								/>
+							</div>
+						</div>
+					</div>
+					{/* photo */}
+					<div className='order-1 xl:order-none mb-8 xl:mb-0'>
+						<Photo />
+					</div>
+				</div>
+			</div>
+			<Stats />
+		</section>
+	);
 };
 
 export default Home;
